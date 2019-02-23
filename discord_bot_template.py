8671eec3c90d2e517d220b95ba07d9224b1ac1fc
Misfitx <47735556+Misfitx@users.noreply.github.com>
2019-02-23 09:01:46 -0500

    Update discord_bot_template.py

--- discord_bot_template.py
+++ discord_bot_template.py
@@ -1,25 +1,24 @@
 from discord.ext import commands
 secretkey='yourkey'
-bot_prefix=''
+bot_prefix='#'
 client=commands.Bot(command_prefix=bot_prefix)
 
 def my_function(string):
-    if string=='Hello':
-        return [True,'Hey']
-    else:
-        return [False,None]
+if x in ['bastard','cunt','fuck','whore','shit','bitch','mother fucker','blowjob','dick','slut','cock']:
+    return 'No profanity allowed. If you continue you will be kicked or possibly banned'                                                
+elif x in ["Hello","Hi"]:
+    return 'Hi there, how are you?'
+elif x in ["God\'s not real","God is fake"]:
+    return 'no u'
+else:
+    return ''
+   
 
 @client.event
 async def on_message(message):
-    embedflag=0
-    outputstring=''
     if message.author==client.user:
         pass
     else:
         reply=my_function(message.content)
-        if reply[0]==True:
-            outputstring=reply[1]
-        else:
-            pass
-        await client.send_message(message.channel,outputstring)
+        await client.send_message(message.channel,reply)
 client.run(secretkey)
