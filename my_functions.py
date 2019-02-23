f4d57509ec5276252947e5f1811f62c988858d7e
suneeshjacob <suneeshjacob@gmail.com>
2019-02-23 19:47:59 +0530

    Create my_functions.py

---
+++ my_functions.py
@@ -0,0 +1,9 @@
+def my_function(x):
+    if x in ['bastard','cunt','fuck','whore','shit','bitch','mother fucker','blowjob','dick','slut','cock']:
+        return 'No profanity allowed. If you continue you will be kicked or possibly banned.'                                                
+    elif x in ["Hello","Hi"]:
+        return 'Hi there, how are you?'
+    elif x in ["God\'s not real","God is fake"]:
+        return 'no u'
+    else:
+        return ''
