a97818e34a967e79e7e6f6d8652b126f064b8a36
suneeshjacob <suneeshjacob@gmail.com>
2019-02-23 19:43:05 +0530

    Update discord_bot_template.py

--- discord_bot_template.py
+++ discord_bot_template.py
@@ -3,16 +3,7 @@ secretkey='yourkey'
 bot_prefix=''
 client=commands.Bot(command_prefix=bot_prefix)
 
-def my_function(x):
-    if x in ['bastard','cunt','fuck','whore','shit','bitch','mother fucker','blowjob','dick','slut','cock']:
-        return 'No profanity allowed. If you continue you will be kicked or possibly banned.'                                                
-    elif x in ["Hello","Hi"]:
-        return 'Hi there, how are you?'
-    elif x in ["God\'s not real","God is fake"]:
-        return 'no u'
-    else:
-        return ''
-   
+from my_functions import my_function
 
 @client.event
 async def on_message(message):
