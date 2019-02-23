3e7ec75aa388fa257181337a30fba57bfe420b22
suneeshjacob <suneeshjacob@gmail.com>
2019-02-23 17:46:38 +0530

    Update discord_bot_template.py

--- discord_bot_template.py
+++ discord_bot_template.py
@@ -5,19 +5,23 @@ Client=discord.Client
 bot_prefix=''
 client=commands.Bot(command_prefix=bot_prefix)
 
+def my_function(string):
+    if string=='Hello':
+        return [True,'Hey']
+    else:
+        return [False,None]
 
 @client.event
 async def on_message(message):
     embedflag=0
+    outputstring=''
     if message.author==client.user:
         pass
     else:
-        a,b=message.content.split(',')
-        if int(a) > int(b):
-            outputstring="a is larger than b"
-        elif int(a) == int(b):
-            outputstring="a is equal to b"
+        reply=my_function(message.content)
+        if reply[0]==True:
+            outputstring=reply[1]
         else:
-            outputstring="a is smaller than b"
+            pass
         await client.send_message(message.channel,outputstring)
 client.run(secretkey)
