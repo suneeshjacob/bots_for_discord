25706b1ed68cd166ce0d042efb4e12003e5d1a06
Misfitx <47735556+Misfitx@users.noreply.github.com>
2019-02-23 09:08:48 -0500

    Update discord_bot_template.py

--- discord_bot_template.py
+++ discord_bot_template.py
@@ -5,7 +5,7 @@ client=commands.Bot(command_prefix=bot_prefix)
 
 def my_function(x):
     if x in ['bastard','cunt','fuck','whore','shit','bitch','mother fucker','blowjob','dick','slut','cock']:
-        return 'No profanity allowed. If you continue you will be kicked or possibly banned'                                                
+        return 'No profanity allowed. If you continue you will be kicked or possibly banned.'                                                
     elif x in ["Hello","Hi"]:
         return 'Hi there, how are you?'
     elif x in ["God\'s not real","God is fake"]:
