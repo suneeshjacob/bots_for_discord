394a2cd756ebd6e049ea029d9e4667ebd5717bf3
Misfitx <47735556+Misfitx@users.noreply.github.com>
2019-02-23 09:52:36 -0500

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -1,15 +1,18 @@
 def my_function(x):
-    if x in ['bastard','cunt','fuck','whore','shit','bitch','mother fucker','blowjob','dick','slut','cock']:
-        return 'No profanity allowed. If you continue you will be kicked or possibly banned.'                                                
+    if x in ['bastard','cunt','fuck','whore','shit','bitch','mother fucker','blowjob','dick','slut','cock','asshole','pussy','faggot','nigger','kike','sand nigger']:
+        return 'No profanity or racial slurs allowed. If you continue you will be kicked or possibly banned.'                                                
     elif x in ["Hello","Hi"]:
         return 'Hi there, how are you?'
-    elif x in ["God\'s not real","God is fake"]:
+    elif x in ["God\'s not real.","God is fake."]:
         return 'no u'
-    elif x in ["I guess"]:
-        return 'I know you son of a cow train'
+    elif x in ["I guess."]:
+        return 'I know, you son of a cow train.'
     elif x in ["If I'm not wrong"]:
         return 'You are wrong!'
     elif x in ["Em"]:
         return 'Bow before your queen'
+    elif x in ["God bless you."]:
+        return 'God bless you too!'
+    elif x in ["imaginary sky daddy","jfc","brainwashed"
     else:
         return ''
